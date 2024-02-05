5690e3da806d4c0fa75056e11fc7d95d77662e41
Nathan Cunha <nathancunha38@gmail.com>
2024-02-05 19:12:56 -0300

    Inicializando o repositório

---
+++ src/main/analiz/Analizador.java
@@ -0,0 +1,11 @@
+package analiz;
+/**
+ * Analizador
+ */
+public class Analizador {
+
+
+    public int[] acha_sequencia(double[] probs, int[] valores){
+        return new int[valores.length];
+    }
+}
