9c75da652d91fefb831ed17fae3fb846520038ef
Nathan Cunha <nathancunha38@gmail.com>
2024-02-05 22:51:47 -0300

    Código completo

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,9 +3,9 @@ import analiz.Analizador;
 public class Main {
     public static void main(String[] args) {
         Analizador analizador = new Analizador();
-        int[] valores = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        double[] probs = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int[] resultado = analizador.acha_sequencia(probs ,valores);
+        int[] valores = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        double[] probs = { 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 };
+        int[] resultado = analizador.acha_sequencia(probs, valores);
         for (int i = 0; i < resultado.length; i++) {
             System.out.println(resultado[i]);
         }
--- src/main/analiz/Analizador.java
+++ src/main/analiz/Analizador.java
@@ -1,11 +1,85 @@
 package analiz;
+
 /**
  * Analizador
  */
 public class Analizador {
 
+    public int[] acha_sequencia(double[] probs, int[] valores) {
+        if (valores.length < 1) {
+            int[] aux = { 0 };
+            return aux;
+        }
+        int[] sequencia = new int[valores.length];
+        int promissor = 0;
+        int valorPromissor = 0;
+        double pesoPromissor = (1 - probs[0]) / valores[0];
 
-    public int[] acha_sequencia(double[] probs, int[] valores){
-        return new int[valores.length];
+        for (int i = 0; i < valores.length; i++) {
+            double aux = (1 - probs[i]) / valores[i];
+            if (aux < pesoPromissor) {
+                promissor = i;
+                valorPromissor = valores[i];
+                pesoPromissor = aux;
+            }
+        }
+
+        pesoPromissor = 1 - pesoPromissor;
+        adicionarPromissorSequencia(sequencia, 0, promissor);
+        probs[promissor] = -1;
+        valores[promissor] = -1;
+        int j = 1;
+        for (int i = 0; i < valores.length; i++) {
+            if (valores[i] >= 0) {
+
+                int candidato = promissor(valores, probs, pesoPromissor, valorPromissor);
+                pesoPromissor = calculaPesoPromissor(pesoPromissor, probs, candidato);
+                valorPromissor = calculaValorPromissor(valores, candidato, valorPromissor);
+                adicionarPromissorSequencia(sequencia, j, candidato);
+                probs[candidato] = -1;
+                valores[candidato] = -1;
+                j++;
+                i--;
+            }
+        }
+
+        return sequencia;
+    }
+
+    private void adicionarPromissorSequencia(int[] sequencia, int i, int promissor) {
+        sequencia[i] = promissor;
+    }
+
+    private int calculaValorPromissor(int[] valores, int promissor, int valorPromissorAnterior) {
+        int valorPromissorAtual = valores[promissor];
+        return valorPromissorAnterior + valorPromissorAtual;
+    }
+
+    private double calculaPesoPromissor(double pesoPromissorAnterior, double[] probs, int promissor) {
+
+        return pesoPromissorAnterior * probs[promissor];
+
+    }
+
+    private int promissor(int[] valores, double[] probs, double pesoPromissorAnterior, double valorPromissorAnterior) {
+        double pesoPromissor = 0;
+        int promissor = 0;
+        for (int i = 0; i <= valores.length; i++) {
+            if (valores[i] >= 0) {
+                pesoPromissor = (pesoPromissorAnterior * (1 - probs[i])) / (valorPromissorAnterior + valores[i]);
+                promissor = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < valores.length; i++) {
+            if (valores[i] >= 0) {
+                double aux = (pesoPromissorAnterior * (1 - probs[i])) / (valorPromissorAnterior + valores[i]);
+                if (aux < pesoPromissor) {
+                    promissor = i;
+                }
+            }
+        }
+        return promissor;
     }
 }
